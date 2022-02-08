01b3263fe6e5436ce75f15552cdf7154e2ea6f4f
mattyfivem <99237278+mattyfivem@users.noreply.github.com>
2022-02-08 05:57:26 +0000

    add mw files tweak .lua

--- __resource.lua
+++ __resource.lua
@@ -1,18 +1,7 @@
 
 
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
-client_scripts {
-	'client.lua'
-}
 
-files {
-    'index.html',
-    'style.css',
-    'song.mp3',
-    'video.mp4'
-}
 
-loadscreen 'video.mp4'
 
-ui_page 'ui.html'
+this_is_a_map 'yes'
