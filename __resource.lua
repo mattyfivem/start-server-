ee435695439ab9c38535b158a00461ebc91a0e88
mattyfivem <99237278+mattyfivem@users.noreply.github.com>
2022-02-08 05:33:57 +0000

    small tweek

---
+++ __resource.lua
@@ -0,0 +1,18 @@
+
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+client_scripts {
+	'client.lua'
+}
+
+files {
+    'index.html',
+    'style.css',
+    'song.mp3',
+    'video.mp4'
+}
+
+loadscreen 'video.mp4'
+
+ui_page 'ui.html'
